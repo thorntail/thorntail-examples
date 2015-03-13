9ef974b56db17827ac9c00d81176be172e5236a1
Bob McWhirter <bob@mcwhirter.org>
2015-03-13 14:57:43 -0400

    Adjust the POM for the plugin. Rework main() a bit.

--- src/main/java/org/projectodd/myapp/Main.java
+++ src/main/java/org/projectodd/myapp/Main.java
@@ -1,9 +1,15 @@
 package org.projectodd.myapp;
 
-import org.wildfly.selfcontained.Container;
-import org.wildfly.selfcontained.IoSubsystem;
-import org.wildfly.selfcontained.LoggingSubsystem;
-import org.wildfly.selfcontained.UndertowSubsystem;
+
+import org.wildfly.boot.container.Container;
+import org.wildfly.boot.container.SocketBindingGroup;
+import org.wildfly.boot.core.LoggingSubsystem;
+import org.wildfly.boot.core.RequestControllerSubsystem;
+import org.wildfly.boot.web.EeSubsystem;
+import org.wildfly.boot.web.IoSubsystem;
+import org.wildfly.boot.web.NamingSubsystem;
+import org.wildfly.boot.web.SecuritySubsystem;
+import org.wildfly.boot.web.UndertowSubsystem;
 
 /**
  * @author Bob McWhirter
@@ -14,16 +20,22 @@ public class Main {
         Container container = new Container();
 
         container
+                .subsystem(new NamingSubsystem())
+                .subsystem(new EeSubsystem())
+                .subsystem(new SecuritySubsystem())
+                .subsystem(new RequestControllerSubsystem())
                 .subsystem(new LoggingSubsystem()
                                 .formatter("PATTERN", "%d{yyyy-MM-dd HH:mm:ss,SSS} %-5p [%c] (%t) %s%e%n")
-                                .consoleHandler("TRACE", "PATTERN")
-                                .rootLogger("CONSOLE", "TRACE")
+                                .consoleHandler("INFO", "PATTERN")
+                                .rootLogger("CONSOLE", "INFO")
                 )
                 .subsystem(new IoSubsystem())
                 .subsystem(new UndertowSubsystem())
                 .iface("public", "${jboss.bind.address:127.0.0.1}")
-                .socketBindingGroup("default-sockets", "public", "0")
-                .socketBinding("http", 8080).end()
+                .socketBindingGroup(
+                        new SocketBindingGroup("default-sockets", "public", "0")
+                                .socketBinding("http", 8080)
+                )
                 .start();
     }
 }
