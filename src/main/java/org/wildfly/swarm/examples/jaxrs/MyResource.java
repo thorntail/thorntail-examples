cda087ea37660f5d763de67451d8a5a66ca08526
Bob McWhirter <bob@mcwhirter.org>
2015-04-16 16:53:39 -0400

    Include an application-specific dependency to test out WEB-INF/lib/**

--- src/main/java/org/wildfly/swarm/examples/jaxrs/MyResource.java
+++ src/main/java/org/wildfly/swarm/examples/jaxrs/MyResource.java
@@ -1,5 +1,7 @@
 package org.wildfly.swarm.examples.jaxrs;
 
+import org.joda.time.DateTime;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -13,6 +15,6 @@ public class MyResource {
     @GET
     @Produces( "text/plain" )
     public String get() {
-        return "Howdy!";
+        return "Howdy at " + new DateTime();
     }
 }
