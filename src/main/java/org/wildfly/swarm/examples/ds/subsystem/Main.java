d1d5e43f16db9eb37af7fd8e9fe8938134a4cb39
Bob McWhirter <bob@mcwhirter.org>
2015-04-30 11:36:45 -0400

    Subsystem/Fraction configuration of a driver & datasource.

---
+++ src/main/java/org/wildfly/swarm/examples/ds/subsystem/Main.java
@@ -0,0 +1,40 @@
+package org.wildfly.swarm.examples.ds.subsystem;
+
+import org.wildfly.swarm.container.Container;
+import org.wildfly.swarm.datasources.Datasource;
+import org.wildfly.swarm.datasources.DatasourcesFraction;
+import org.wildfly.swarm.datasources.Driver;
+import org.wildfly.swarm.jaxrs.JaxRsDeployment;
+
+/**
+ * @author Bob McWhirter
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Container container = new Container();
+
+        // Configure the Datasources subsystem with a driver
+        // and a datasource
+        container.subsystem(new DatasourcesFraction()
+                        .driver(new Driver("h2")
+                                .xaDatasourceClassName("org.h2.jdbcx.JdbcDataSource")
+                                .module("com.h2database.h2"))
+                        .datasource(new Datasource("ExampleDS")
+                                .driver("h2")
+                                .connectionURL("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
+                                .authentication("sa", "sa"))
+        );
+
+
+        // Start the container
+        container.start();
+
+        JaxRsDeployment appDeployment = new JaxRsDeployment();
+        appDeployment.addResource(MyResource.class);
+
+        // Deploy your app
+        container.deploy(appDeployment);
+
+    }
+}
