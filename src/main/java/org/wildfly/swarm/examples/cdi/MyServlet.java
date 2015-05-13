9635639dfd43891a4833325330f34ec0550a1aed
Ken Finnigan <ken@kenfinnigan.me>
2015-05-12 20:33:51 -0400

    Example of CDI application using Servlets

---
+++ src/main/java/org/wildfly/swarm/examples/cdi/MyServlet.java
@@ -0,0 +1,25 @@
+package org.wildfly.swarm.examples.cdi;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Ken Finnigan
+ */
+@WebServlet(name = "MyServlet", urlPatterns = "/*")
+public class MyServlet extends HttpServlet {
+
+    @Inject
+    Greeter greeter;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write(greeter.greet());
+    }
+}
