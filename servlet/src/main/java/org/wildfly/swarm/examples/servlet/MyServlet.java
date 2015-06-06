3e423cec49617164e88628f0c3fd28c696a8c775
arun-gupta <arun.gupta@gmail.com>
2015-06-05 23:03:26 -0700

    removing web.xml and making the samples Java EE 7 compliant

--- servlet/src/main/java/org/wildfly/swarm/examples/servlet/MyServlet.java
+++ servlet/src/main/java/org/wildfly/swarm/examples/servlet/MyServlet.java
@@ -3,6 +3,7 @@ package org.wildfly.swarm.examples.servlet;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +13,7 @@ import org.joda.time.DateTime;
 /**
  * @author Bob McWhirter
  */
+@WebServlet("/*")
 public class MyServlet extends HttpServlet {
 
     @Override
