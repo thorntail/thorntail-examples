7423cb55e381978a0d495afd60406907680aadfc
Ken Finnigan <ken@kenfinnigan.me>
2015-06-17 15:48:19 -0400

    Fix up example to prevent JPAFraction from defaulting datasources

--- jpa-jaxrs-cdi-shrinkwrap/src/main/java/org/wildfly/swarm/examples/jpa/Main.java
+++ jpa-jaxrs-cdi-shrinkwrap/src/main/java/org/wildfly/swarm/examples/jpa/Main.java
@@ -3,9 +3,10 @@ package org.wildfly.swarm.examples.jpa;
 import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
 import org.wildfly.swarm.container.Container;
 import org.wildfly.swarm.datasources.Datasource;
-import org.wildfly.swarm.datasources.DatasourceDeployment;
-import org.wildfly.swarm.datasources.DriverDeployment;
+import org.wildfly.swarm.datasources.DatasourcesFraction;
+import org.wildfly.swarm.datasources.Driver;
 import org.wildfly.swarm.jaxrs.JAXRSDeployment;
+import org.wildfly.swarm.jpa.JPAFraction;
 
 /**
  * @author Ken Finnigan
@@ -13,20 +14,25 @@ import org.wildfly.swarm.jaxrs.JAXRSDeployment;
 public class Main {
     public static void main(String[] args) throws Exception {
         Container container = new Container();
-        container.start();
 
-        // Create a JDBC driver deployment using maven groupId:artifactId
-        // The version is resolved from your pom.xml's <dependency>
-        DriverDeployment driverDeployment = new DriverDeployment(container, "com.h2database:h2", "h2");
-        container.deploy(driverDeployment);
-
-        // Create a DS deployment
-        DatasourceDeployment dsDeployment = new DatasourceDeployment(container, new Datasource("ExampleDS")
-                .connectionURL("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
-                .driver("h2")
-                .authentication("sa", "sa")
+        container.subsystem(new DatasourcesFraction()
+                        .driver(new Driver("h2")
+                                .datasourceClassName("org.h2.Driver")
+                                .xaDatasourceClassName("org.h2.jdbcx.JdbcDataSource")
+                                .module("com.h2database.h2"))
+                        .datasource(new Datasource("MyDS")
+                                .driver("h2")
+                                .connectionURL("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
+                                .authentication("sa", "sa"))
         );
-        container.deploy(dsDeployment);
+
+        // Prevent JPA Fraction from installing it's default datasource fraction
+        container.fraction(new JPAFraction()
+                        .inhibitDefaultDatasource()
+                        .defaultDatasourceName("MyDS")
+        );
+
+        container.start();
 
         JAXRSDeployment deployment = new JAXRSDeployment(container);
         deployment.getArchive().addClasses(Employee.class);
