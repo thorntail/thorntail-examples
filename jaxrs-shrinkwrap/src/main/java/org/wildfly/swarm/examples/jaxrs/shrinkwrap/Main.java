afebcc98da61c8c4537c8362c5b8eeb5e99e4c31
Bob McWhirter <bob@mcwhirter.org>
2015-07-17 09:25:36 -0400

    Start adjusting to a pure ShrinkWrap lifestyle.

--- jaxrs-shrinkwrap/src/main/java/org/wildfly/swarm/examples/jaxrs/shrinkwrap/Main.java
+++ jaxrs-shrinkwrap/src/main/java/org/wildfly/swarm/examples/jaxrs/shrinkwrap/Main.java
@@ -1,7 +1,7 @@
 package org.wildfly.swarm.examples.jaxrs.shrinkwrap;
 
 import org.wildfly.swarm.container.Container;
-import org.wildfly.swarm.jaxrs.JAXRSDeployment;
+import org.wildfly.swarm.jaxrs.JAXRSArchive;
 
 /**
  * @author Bob McWhirter
@@ -12,12 +12,24 @@ public class Main {
 
         Container container = new Container();
 
+        JAXRSArchive archive = container.create( "myapp.war", JAXRSArchive.class );
+        archive.addResource(MyResource.class);
+        archive.addClass( MyApp.class );
+        archive.addClass( MyApp.class.getName() );
+        archive.addModule( "com.foo.bar", "main" );
+        archive.addModule( "com.foo.bar", "taco" );
+        container.start().deploy( archive );
+
+        /*
+
         JAXRSDeployment deployment = new JAXRSDeployment(container);
         deployment.addResource(MyResource.class);
 
         System.err.println( javax.security.auth.login.LoginException.class );
 
         container.start().deploy(deployment);
+        */
+
 
     }
 }
