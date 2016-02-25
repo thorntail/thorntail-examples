091a759c68bd66155905f52573b1b1dcd12a03fb
Ken Finnigan <ken@kenfinnigan.me>
2016-02-25 13:40:44 -0500

    Fix issues with Spring example not starting without error

--- spring/spring-data/src/main/java/org/wildfly/swarm/examples/springdata/repository/EmployeeRepository.java
+++ spring/spring-data/src/main/java/org/wildfly/swarm/examples/springdata/repository/EmployeeRepository.java
@@ -1,12 +1,10 @@
 package org.wildfly.swarm.examples.springdata.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.wildfly.swarm.examples.springdata.model.Employee;
 
 /**
  * @author Ken Finnigan
  */
-@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
