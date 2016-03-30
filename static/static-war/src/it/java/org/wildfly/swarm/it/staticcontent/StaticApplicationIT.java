d2bfa3ccebc36fc84a20c7c59592ebb9a5b162f7
Ben Browning <bbrownin@redhat.com>
2016-03-30 14:26:57 -0400

    Test that static file changes are reloaded in static-war example
    This is a test for SWARM-230 to ensure that static file changes do get
    reflected immediately when refreshing the browser.

--- static/static-war/src/it/java/org/wildfly/swarm/it/staticcontent/StaticApplicationIT.java
+++ static/static-war/src/it/java/org/wildfly/swarm/it/staticcontent/StaticApplicationIT.java
@@ -1,5 +1,10 @@
 package org.wildfly.swarm.it.staticcontent;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Test;
@@ -19,8 +24,26 @@ public class StaticApplicationIT extends AbstractIntegrationTest {
     WebDriver browser;
 
     @Test
-    public void testIt() {
+    public void testIt() throws Exception {
         browser.navigate().to("http://localhost:8080/");
         assertThat(browser.getPageSource()).contains("Howdy!");
+
+        // This test only works when run from Maven anyway, so go ahead and
+        // assume the system property we set in Maven is always there
+        Path testWebDir = Paths.get(System.getProperty("test.web.dir"));
+        Path indexPath = testWebDir.resolve("index.html");
+        Path fooPath = testWebDir.resolve("foo.html");
+
+        try {
+            Files.copy(indexPath, fooPath, StandardCopyOption.REPLACE_EXISTING);
+            browser.navigate().to("http://localhost:8080/foo.html");
+            assertThat(browser.getPageSource()).contains("Howdy!");
+
+            Files.write(fooPath, "Howdy from foo!".getBytes());
+            browser.navigate().refresh();
+            assertThat(browser.getPageSource()).contains("Howdy from foo!");
+        } finally {
+             Files.delete(fooPath);
+        }
     }
 }
