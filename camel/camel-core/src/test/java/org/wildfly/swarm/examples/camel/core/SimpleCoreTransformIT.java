ffdfb905dd0c16c1f16786058d255b84c3058e93
Thomas Diesler <thomas.diesler@jboss.com>
2016-04-19 11:11:14 +0200

    [SWARM-444] Add example for camel-core

---
+++ camel/camel-core/src/test/java/org/wildfly/swarm/examples/camel/core/SimpleCoreTransformIT.java
@@ -0,0 +1,38 @@
+/*
+ * #%L
+ * Wildfly Swarm :: Examples :: Camel CDI
+ * %%
+ * Copyright (C) 2016 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.wildfly.swarm.examples.camel.core;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.swarm.it.AbstractIntegrationTest;
+import org.wildfly.swarm.it.Log;
+
+@RunWith(Arquillian.class)
+public class SimpleCoreTransformIT extends AbstractIntegrationTest {
+
+	@Test
+	public void testIt() throws Exception {
+		
+		Log log = getStdOutLog();
+
+		assertThatLog(log).hasLineContaining("(CamelContext: simpleTransform) started");
+	}
+}
