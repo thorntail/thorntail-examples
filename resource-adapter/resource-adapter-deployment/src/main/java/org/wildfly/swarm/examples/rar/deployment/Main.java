598c88dc849e9415ec9c4016ff96a506c4839fb2
Bob McWhirter <bob@mcwhirter.org>
2016-05-23 15:29:58 -0400

    Ignore diskHome/. Exchanges tabs for spaces in pom.xml. Use assets for providing ironjacamar.xml instead of files. Use JARArchive instead of JavaArchive.

--- resource-adapter/resource-adapter-deployment/src/main/java/org/wildfly/swarm/examples/rar/deployment/Main.java
+++ resource-adapter/resource-adapter-deployment/src/main/java/org/wildfly/swarm/examples/rar/deployment/Main.java
@@ -3,11 +3,13 @@ package org.wildfly.swarm.examples.rar.deployment;
 import java.io.File;
 
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.wildfly.swarm.Swarm;
 import org.wildfly.swarm.container.Container;
 import org.wildfly.swarm.ejb.EJBFraction;
 import org.wildfly.swarm.resource.adapters.RARArchive;
+import org.wildfly.swarm.spi.api.JARArchive;
 
 /**
  * @author Ralf Battenfeld
@@ -16,22 +18,20 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         final ClassLoader classLoader = Main.class.getClassLoader();
-        final File raFile = new File(classLoader.getResource("ra.xml").getFile());
-        final File ironJacamarFile = new File(classLoader.getResource("ironjacamar.xml").getFile());
         final Container container = new Container();
         container.fraction(EJBFraction.createDefaultFraction());
         container.start();
-        
+
         // since Swarm.artifact doesn't support rar archives, we have to manually create a rar
         final RARArchive rarArchive = ShrinkWrap.create(RARArchive.class, "xadisk.rar");
         rarArchive.addAsLibrary(Swarm.artifact("net.java.xadisk:xadisk:jar:1.2.2"));
-        rarArchive.addAsManifestResource(raFile, "ra.xml");
-        
+        rarArchive.addAsManifestResource(new ClassLoaderAsset( "ra.xml", classLoader ), "ra.xml");
+
         // add the required ironjacamar.xml descriptor
-        rarArchive.resourceAdapter(ironJacamarFile);
+        rarArchive.resourceAdapter(new ClassLoaderAsset( "ironjacamar.xml", classLoader ));
         container.deploy(rarArchive);
 
-        final JavaArchive appDeployment = ShrinkWrap.create(JavaArchive.class);
+        final JARArchive appDeployment = ShrinkWrap.create(JARArchive.class);
         appDeployment.merge(Swarm.artifact("net.java.xadisk:xadisk:jar:1.2.2"));
         appDeployment.addClass(FileIOBean.class);
 
