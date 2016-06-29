6a4808031be1ab68ba901f366cec15e984e5da88
George Gastaldi <gegastaldi@gmail.com>
2016-06-29 14:53:55 -0300

    Added IT test for Management console

---
+++ management-console/src/test/java/org/wildfly/swarm/examples/management/console/ManagementConsoleIT.java
@@ -0,0 +1,26 @@
+package org.wildfly.swarm.examples.management.console;
+
+import org.fest.assertions.Assertions;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.wildfly.swarm.it.AbstractIntegrationTest;
+import org.wildfly.swarm.it.Log;
+
+@RunWith(Arquillian.class)
+public class ManagementConsoleIT extends AbstractIntegrationTest {
+
+    @Drone
+    WebDriver browser;
+
+    @Test
+    public void testIt() throws Exception {
+        Log log = getStdOutLog();
+        assertThatLog(log).hasLineContaining("Registered web context: /console");
+
+        browser.navigate().to("http://127.0.0.1:8080/console");
+        Assertions.assertThat(browser.getTitle()).isEqualTo("Management Interface");
+    }
+}
