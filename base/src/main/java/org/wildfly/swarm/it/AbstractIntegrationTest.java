6142afaf576606582f147a8d6838647ae5b08067
George Gastaldi <gegastaldi@gmail.com>
2016-06-29 14:42:12 -0300

    Simplified AbstractIntegrationTest.getLog

--- base/src/main/java/org/wildfly/swarm/it/AbstractIntegrationTest.java
+++ base/src/main/java/org/wildfly/swarm/it/AbstractIntegrationTest.java
@@ -1,12 +1,8 @@
 package org.wildfly.swarm.it;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.file.Files;
 
 /**
  * @author Bob McWhirter
@@ -14,31 +10,18 @@ import java.util.List;
 public class AbstractIntegrationTest {
 
     protected Log getStdOutLog() throws Exception {
-        return getLog( "target/stdout.log" );
+        return getLog("target/stdout.log");
     }
 
     protected Log getStdErrLog() throws Exception {
-        return getLog( "target/stderr.log" );
+        return getLog("target/stderr.log");
     }
 
     protected Log getLog(String path) throws IOException {
-
-        FileInputStream in = new FileInputStream( new File( path ) );
-
-        BufferedReader reader = new BufferedReader( new InputStreamReader( in ) );
-
-        String line = null;
-
-        List<String> lines = new ArrayList<>();
-
-        while ( ( line = reader.readLine() ) != null ) {
-            lines.add( line );
-        }
-
-        return new Log(lines);
+        return new Log(Files.readAllLines(new File(path).toPath()));
     }
 
     public LogAssert assertThatLog(Log log) {
-        return new LogAssert( log );
+        return new LogAssert(log);
     }
 }
