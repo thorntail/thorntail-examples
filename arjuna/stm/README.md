995533c8e98d1f1706f9915a89778d1ba1e140da
Mark Little <mlittle@redhat.com>
2017-12-25 08:17:25 +0000

    Added another STM example.
    This one shows persistent invocations.

--- arjuna/stm/README.md
+++ arjuna/stm/README.md
@@ -78,3 +78,44 @@ Transactional object value incremented to 17
 Transactional object value incremented to 18
 Transactional object value incremented to 19
 Transactional object value incremented to 20
+
+What's happening is that we've created a recoverable STM-managed object (an integer state) within WildFly Swarm and given it an initial value
+of 10; then we increment it by another 10, printing out each time we do.
+
+If you repeat the operation you will see a new instance created and the end state will always be 20.
+
+Now try ...
+
+http://localhost:8080/stmPersistent
+
+The first time you do will look similar to the above:
+
+Object name: 0:ffffc0a80102:-48ffece4:5a3fb241:5d
+Transactional object value initially 20
+Transactional value incremented to 21
+Transactional value incremented to 22
+Transactional value incremented to 23
+Transactional value incremented to 24
+Transactional value incremented to 25
+Transactional value incremented to 26
+Transactional value incremented to 27
+Transactional value incremented to 28
+Transactional value incremented to 29
+Transactional value incremented to 30
+
+But give it a try again ...
+
+Object name: 0:ffffc0a80102:-48ffece4:5a3fb241:5d
+Transactional object value initially 30
+Transactional value incremented to 31
+Transactional value incremented to 32
+Transactional value incremented to 33
+Transactional value incremented to 34
+Transactional value incremented to 35
+Transactional value incremented to 36
+Transactional value incremented to 37
+Transactional value incremented to 38
+Transactional value incremented to 39
+Transactional value incremented to 40
+
+Now you can see that each time the invocation happens we re-use the same object and the integer value just keeps increasing.
--- arjuna/stm/src/main/java/org/wildfly/swarm/examples/stm/MyResource.java
+++ arjuna/stm/src/main/java/org/wildfly/swarm/examples/stm/MyResource.java
@@ -26,6 +26,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import com.arjuna.ats.arjuna.common.Uid;
 import com.arjuna.ats.arjuna.AtomicAction;
 import org.jboss.stm.Container;
 
@@ -42,8 +43,8 @@ public class MyResource {
         return "Active";
     }
 
-@Path("stm")
-	@GET
+    @Path("stm")
+    @GET
     @Produces("text/plain")
     public String stm() throws Exception  // dummy method name for now
     {
@@ -51,10 +52,12 @@ public class MyResource {
 	 * STM states are identified by Uids in the ObjectStore. This is an example.
 	 */
 
-	Container<Sample> theContainer = new Container<Sample>("Demo", Container.TYPE.PERSISTENT, Container.MODEL.SHARED);
+	Container<Sample> theContainer = new Container<Sample>("Demo", Container.TYPE.RECOVERABLE, Container.MODEL.EXCLUSIVE);
 	Sample obj1 = theContainer.create(new SampleLockable(10));
 	String str = "Object name: "+theContainer.getIdentifier(obj1)+"\n";
 
+	str += "Transactional object value initially "+obj1.value()+"\n";
+	
 	for (int i = 0; i < 10; i++) {
 	    AtomicAction A = new AtomicAction();
 
@@ -62,11 +65,52 @@ public class MyResource {
 
 	    obj1.increment();
 
-	    str += "Transaction value hello" + obj1.value() + "\n";
+	    str += "Transactional object value incremented to " + obj1.value() + "\n";
 
 	    A.commit();
 	}
 
 	return str;
     }
+
+    @Path("stmPersistent")
+    @GET
+    @Produces("text/plain")
+    public String stmPersistent() throws Exception  // dummy method name for now
+    {
+	/*
+	 * STM states are identified by Uids in the ObjectStore. This is an example.
+	 */
+
+	Container<Sample> theContainer = new Container<Sample>("Demo", Container.TYPE.PERSISTENT, Container.MODEL.EXCLUSIVE);
+	Sample obj1 = ((MyResource._theUid == null) ? theContainer.create(new SampleLockable(10)) : theContainer.clone(new SampleLockable(-1), MyResource._theUid));
+	String str = "Object name: "+theContainer.getIdentifier(obj1)+"\n";
+
+	AtomicAction B = new AtomicAction();
+
+	B.begin();
+	
+	str += "Transactional object value initially "+obj1.value()+"\n";
+	
+	B.commit();
+	
+	if (MyResource._theUid == null)
+	    MyResource._theUid = theContainer.getIdentifier(obj1);
+	
+	for (int i = 0; i < 10; i++) {
+	    AtomicAction A = new AtomicAction();
+
+	    A.begin();
+
+	    obj1.increment();
+
+	    str += "Transactional value incremented to " + obj1.value() + "\n";
+
+	    A.commit();
+	}
+
+	return str;
+    }    
+
+    private static Uid _theUid = null;
 }
